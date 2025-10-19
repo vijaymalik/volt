b78721a0f8014e471f8ca2d9d817c004aa77a416
ayushsahu <ayush@peregrine-it.com>
2025-10-19 18:19:15 +0530

    new theme

--- app/public/wp-content/themes/volt-theme/page-contact-us.php
+++ app/public/wp-content/themes/volt-theme/page-contact-us.php
@@ -851,7 +851,7 @@
 
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Products </a>
+                        <a class="nav-link active" aria-current="page" href="/">Products </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Recipes </a>
