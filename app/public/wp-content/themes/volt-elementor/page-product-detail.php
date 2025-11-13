247beff5b35ec82ab75b85816ada2f5b7e064a07
baljinderkaur38 <51135094+baljinderkaur38@users.noreply.github.com>
2025-11-13 16:22:35 +0530

    product post type

--- app/public/wp-content/themes/volt-elementor/page-product-detail.php
+++ app/public/wp-content/themes/volt-elementor/page-product-detail.php
@@ -1,22 +1,20 @@
 <?php
 /**
- * Template Name: Product Detail (Volt)
- * Description: Custom Product Detail template
+ * Template Name: Product Detail Page (Volt)
+ * Template Post Type: products
+ * Description: Displays a products with Elementor content and ACF fields.
  */
+get_header();
 ?>
 
-<?php get_header(); ?>
-<div class="volt-template-container">
-
-    <?php
-    // 🔹 Required for Elementor to detect the editable area
-    if (have_posts()):
-        while (have_posts()):
-            the_post();
-            the_content(); // <-- Elementor hooks here
-        endwhile;
-    endif;
-    ?>
-
+<div class="breadcrumb">
+    <a href="<?php echo esc_url(home_url()); ?>">Home</a>
+    <span>/</span>
+    <a href="<?php echo esc_url(site_url('/products')); ?>">Our Products</a>
+    <span>/</span>
+    <span class="active"><?php the_title(); ?></span>
 </div>
+<?php the_content(); ?>
+<?php get_template_part('template-parts/did-you'); ?>
+<?php get_template_part('template-parts/nearby-store'); ?>
 <?php get_footer(); ?>
